1c5add418267db3aac85bdb199776949fcffba5d
Najiba Halim <najiba.r.halim@gmail.com>
2017-10-30 15:37:52 +0530

    LRU DONE

--- lru.c
+++ lru.c
@@ -1,1 +1,49 @@
 #include<stdio.h>
+int cache[3]={-99,-99,-99},memory[100];
+int main(){
+	int i,j,k,n,hit=0,miss=0,h,pointer=0,flag,used[3]={0};
+	printf("Enter the size of input String ");
+	scanf("%d",&n);
+	printf("Enter the input String\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&memory[i]);
+	for(i=0;i<n;i++)
+	{
+		flag=0;
+		for(j=0;j<3;j++)
+		{
+			if(cache[j]==memory[i])
+			{
+				hit++;
+				used[j]++;
+				printf("Status of Cache::");
+				for(h=0;h<3;h++)
+				printf("%d ",cache[h]);
+				printf("HIT\n");
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			pointer=0;
+			for(k=2;k>=0;k--)
+			{
+				if(used[k]<used[pointer] || cache[k]==-99){
+					pointer=k;
+				}
+			}
+			miss++;
+			cache[pointer]=memory[i];
+			used[pointer]=0;
+			printf("Status of Cache:: ");
+			for(h=0;h<3;h++)
+			printf("%d ",cache[h]);
+			printf("MISS\n");
+		}
+	
+	}
+	printf("\nHits=%d Miss=%d hit/miss=%.2f ",hit,miss,(float)hit/miss);
+	return 0;
+	
+}
